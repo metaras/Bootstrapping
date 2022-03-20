ac9fcd92ffb00dd6d212dbcf232f60cd7531e952
Metaras <metaras.c@ku.th>
2022-03-20 16:05:22 +0700

    chapter-5 example-2

--- chapter-5/video-streaming/src/index.js
+++ chapter-5/video-streaming/src/index.js
@@ -1,5 +1,39 @@
 const express = require("express");
 const fs = require("fs");
+const http = require("http");
+
+//
+// Send the "viewed" to the history microservice.
+//
+function sendViewedMessage(videoPath) {
+  const postOptions = { // Options to the HTTP POST request.
+    method: "POST", // Sets the request method as POST.
+    headers: {
+      "Content-Type": "application/json", // Sets the content type for the request's body.
+    },
+  };
+
+  const requestBody = { // Body of the HTTP POST request.
+    videoPath: videoPath
+  };
+
+  const req = http.request( // Send the "viewed" message to the history microservice.
+      "http://history/viewed",
+      postOptions
+  );
+
+  req.on("close", () => {
+    console.log("Sent 'viewed' message to history microservice.");
+  });
+
+  req.on("error", (err) => {
+    console.error("Failed to send 'viewed' message!");
+    console.error(err && err.stack || err);
+  });
+
+  req.write(JSON.stringify(requestBody)); // Write the body to the request.
+  req.end(); // End the request.
+}
 
 //
 // Setup event handlers.
@@ -21,6 +55,8 @@ function setupHandlers(app) {
       });
 
       fs.createReadStream(videoPath).pipe(res);
+
+      sendViewedMessage(videoPath); // Send message to "history" microservice that this video has been "viewed".
     });
   });
 }
@@ -29,7 +65,7 @@ function setupHandlers(app) {
 // Start the HTTP server.
 //
 function startHttpServer() {
-  return new Promise((resolve, reject) => { // Wrap in a promise so we can be notified when the server has started.
+  return new Promise(resolve => { // Wrap in a promise so we can be notified when the server has started.
     const app = express();
     setupHandlers(app);
 
