bd3609984f59627cb718575a6c41bdeba1c26c8b
Metaras <metaras.c@ku.th>
2022-03-27 17:10:33 +0700

    chapter-6 finish

--- chapter-6/scripts/container-registry.tf
+++ chapter-6/scripts/container-registry.tf
@@ -1,9 +1,9 @@
 # Creates a container registry on Azure so that you can publish your Docker images.
 
 resource "azurerm_container_registry" "container_registry" {
-  name                = "flixtube6706"
+  name                = var.app_name
   resource_group_name = azurerm_resource_group.flixtube.name
-  location            = "westus"
+  location            = var.location
   admin_enabled       = true
   sku                 = "Basic"
 }
@@ -18,5 +18,5 @@ output "registry_un" {
 
 output "registry_pw" {
   sensitive = true
-  value = azurerm_container_registry.container_registry.admin_password
+  value =   azurerm_container_registry.container_registry.admin_password
 }
